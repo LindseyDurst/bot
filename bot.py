b60d57498fbe29248d31f99c0d4ee2231c849206
LindseyDurst <shellenav@gmail.com>
2017-02-17 15:44:54 +0300

    all starts at the beginning

---
+++ bot.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import config
+import telebot
+
+bot = telebot.TeleBot(config.token)
+token ="371478199:AAFnBBn2d79tIT0ut6Pj4ZKYHHh8PILbij8"
+#https://api.vk.com/method/wall.get?domain=kaspercareer&count=10&filter=owner
